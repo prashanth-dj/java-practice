29664a01f9a0c6bf1671686253f3ffc0d64ff885
prashanth-dj <prashanth.dvj@gmail>
2024-05-27 15:18:52 +0530

    array comit

--- Demo.java
+++ Demo.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Prashanth");
         int[] arr = new int[5];
+        for(int i =0;i<arr.length;i++){
+            arr[i]=i;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
