3afd2325705e719632c76d3d580c8b1b2f160598
prashanth-dj <prashanth.dvj@gmail>
2024-05-27 17:59:03 +0530

    Added Arrays folder

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i=0;i<arr.length;i++){
+            System.out.println(i);
+        }
+    }
+}
